b2a8d1c306545dd977644116730c53b6e2b96023
fourierz517 <83400846+fourierz517@users.noreply.github.com>
2022-04-09 21:49:12 -0700

    fixed package.json

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express'); //Line 1
+const express = require('express');
 const {DICT, NUMBER_OF_KEYS, dfs, listNames} = require('./trie/trieMethods.js');
 const trie = require('./trie/citiesTrie.json');
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
-const API_ID = process.env.API_ID;
+const app = express();
+const port = process.env.PORT || 5000;
+//const API_ID = process.env.API_ID;
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/listcities', (req, res)=>{
