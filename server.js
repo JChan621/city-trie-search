4e5e73c305f24af15f2056a73ea6f8eaec110f1e
fourierz517 <83400846+fourierz517@users.noreply.github.com>
2022-04-21 14:02:13 -0700

    allow cors

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const trie = require('./trie/citiesTrie.json');
-//var cors = require('cors');
+var cors = require('cors');
 const {listNames} = require('./trie/trieMethods.js');
 const app = express();
 const port = process.env.PORT || 5000;
 //const API_ID = process.env.API_ID;
+// This displays message that the server running and listening to specified port
 
+app.use(cors())
 // create a GET route
 app.get('/listcities', (req, res)=>{
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Headers', "*")
     res.send({data: listNames(trie, req.query.prefix)});
 });
+
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
